cbb95d087f86a6b7acfb333394b793b948022619
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-06-26 17:02:10 +0200

    Commited

--- app/src/main/java/com/example/bmi_calculator/BMICalculate.java
+++ app/src/main/java/com/example/bmi_calculator/BMICalculate.java
@@ -1,4 +1,20 @@
 package com.example.bmi_calculator;
 
 public interface BMICalculate {
+    // getter
+    String getInputHeight();
+    String getInputWeight();
+    double getCalcHeight();
+    double getCalcWeight();
+    double getBmi();
+
+    // setter
+    void setInputHeight(String inputHeight);
+    void setInputWeight(String inputWeight);
+    void setCalcHeight(double calcHeight);
+    void setCalcWeight(double calcWeight);
+    void setBmi(double bmi);
+
+    // wrap methods
+    void callCalcBMI();
 }
--- app/src/main/java/com/example/bmi_calculator/impl/BMICalculate_impl.java
+++ app/src/main/java/com/example/bmi_calculator/impl/BMICalculate_impl.java
@@ -3,9 +3,74 @@ package com.example.bmi_calculator.impl;
 import com.example.bmi_calculator.BMICalculate;
 
 public class BMICalculate_impl implements BMICalculate {
+    private String inputHeight;
+    private String inputWeight;
+    private double calcHeight;
+    private double calcWeight;
+    private double bmi;
 
     public BMICalculate_impl() {
-
+        this.inputHeight = "0";
+        this.inputWeight = "0";
+        this.bmi = 0;
+        this.calcHeight = 0;
+        this.calcWeight = 0;
     }
 
+    // getter
+    @Override
+    public String getInputHeight() {
+        return inputHeight;
+    }
+
+    @Override
+    public String getInputWeight() {
+        return inputWeight;
+    }
+
+    @Override
+    public double getCalcHeight() {
+        return calcHeight;
+    }
+
+    @Override
+    public double getCalcWeight() {
+        return calcWeight;
+    }
+
+    @Override
+    public double getBmi() {
+        return bmi;
+    }
+
+    // setter
+    @Override
+    public void setInputHeight(String inputHeight) {
+        this.inputHeight = inputHeight;
+    }
+
+    @Override
+    public void setInputWeight(String inputWeight) {
+        this.inputWeight = inputWeight;
+    }
+
+    @Override
+    public void setCalcHeight(double calcHeight) {
+        this.calcHeight = Double.parseDouble(inputHeight);
+    }
+
+    @Override
+    public void setCalcWeight(double calcWeight) {
+        this.calcWeight = Double.parseDouble(inputWeight);
+    }
+
+    @Override
+    public void setBmi(double bmi) {
+        this.bmi = bmi;
+    }
+
+    @Override
+    public void callCalcBMI() {
+
+    }
 }
